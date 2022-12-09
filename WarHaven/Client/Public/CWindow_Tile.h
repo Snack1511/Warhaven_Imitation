ccb6bbf36dcff2fee6275ab96e3a0e3640c74b0e
Ditto <77193953+Snack1511@users.noreply.github.com>
2022-12-09 12:33:51 +0900

    Update CWindow_Tile.h

--- WarHaven/Client/Public/CWindow_Tile.h
+++ WarHaven/Client/Public/CWindow_Tile.h
@@ -50,8 +50,12 @@ private:	/*세이브 로드 관련 변수..*/
 	_uint m_iFileDataIndex = 0;
 private:
 	CTile* m_pSelectTile = nullptr;
+	_uint m_iCurSelectTileIndex = 0;
+	_uint m_iCurSelectLayer = 0;
+	_uint m_iCurSelectNeighvor = 0;
+	_bool m_bSelectNeighvor = false;
 private:
 	void		On_Picking(_uint iLayerIndex, _float4 vPickedPos);
-
+	void		On_Pick_Neighvor(_uint iLayerIndex, _float4 vPickedPos);
 };
 END
