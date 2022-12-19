ea103591e955843691de003ab464dab58beb1c55
ChrisOyj <joonbaby1@gmail.com>
2022-12-19 23:32:09 +0900

    Update CLevel_Paden.cpp

--- WarHaven/Client/Private/CLevel_Paden.cpp
+++ WarHaven/Client/Private/CLevel_Paden.cpp
@@ -60,8 +60,8 @@ HRESULT CLevel_Paden::SetUp_Prototypes()
 	function<void(CGameObject*, _uint)> Ready_Object = bind(&CLevel_Paden::Ready_GameObject, this, placeholders::_1, placeholders::_2);
 
 	//주석 걸어논거 풀어서 써라 또 여따 TerrainOnly적지말고
-	//CMap_Loader::Load_Data(wstring(TEXT("Map_Paden")), Ready_Object);
-	CMap_Loader::Load_Data(wstring(TEXT("Map_Paden_TerrainOnly")), Ready_Object);
+	CMap_Loader::Load_Data(wstring(TEXT("Map_Paden")), Ready_Object);
+	//CMap_Loader::Load_Data(wstring(TEXT("Map_Paden_TerrainOnly")), Ready_Object);
 	m_fLoadingFinish = 0.5f;
 
 	/* GameSystem */
@@ -83,8 +83,8 @@ HRESULT CLevel_Paden::Enter()
 	if (FAILED(CGameSystem::Get_Instance()->On_EnterStage()))
 		return E_FAIL;
 
-	/*if (FAILED(CGameSystem::Get_Instance()->Paden_EnvironmentEffect()))
-		return E_FAIL;*/
+	if (FAILED(CGameSystem::Get_Instance()->Paden_EnvironmentEffect()))
+		return E_FAIL;
 
 	return S_OK;
 }
