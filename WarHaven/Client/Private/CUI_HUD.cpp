358d01d79b7310034c704e56aa31765cce0cacd5
DoubleJK <kmeiguitar96@gmail.com>
2023-01-16 05:17:58 +0900

    ㄷㄴㅊ 사운드

--- WarHaven/Client/Private/CUI_HUD.cpp
+++ WarHaven/Client/Private/CUI_HUD.cpp
@@ -96,7 +96,7 @@ void CUI_HUD::My_Tick()
 
 	if (KEY(ESC, TAP))
 	{
-		Play_Sound(L"UI_BtnEvent");
+		Play_Sound(L"UI_Btn_Down_Main");
 
 		_bool bEscActive = m_pUI_EscMenu->Is_Valid();
 		if (!bEscActive)
