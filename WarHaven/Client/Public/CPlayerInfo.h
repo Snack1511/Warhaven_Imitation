c3365640105f3eb6b295b3cff6256f105970f95b
DoubleJK <kmeiguitar96@gmail.com>
2023-01-16 08:23:09 +0900

    피오나 사운드

--- WarHaven/Client/Public/CPlayerInfo.h
+++ WarHaven/Client/Public/CPlayerInfo.h
@@ -31,9 +31,9 @@ return pInstance;\
 #define WARRIOR_TH
 //#define ARCHER_TH
 #define FIONA_TH
-//#define PALADIN_TH
+#define PALADIN_TH
 #define ENGINEER_TH
-#define PRIEST_TH
+//#define PRIEST_TH
 //#define QANDA_TH
 //#define LANCER_TH
 
